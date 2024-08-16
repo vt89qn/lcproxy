631fb6185456a625496f0bc3848c811f8b478b29
Tin Nguyễn <vantin.work@gmail.com>
2024-08-16 10:07:18 +0700

    Update lcproxy.sh

--- lcproxy.sh
+++ lcproxy.sh
@@ -10,7 +10,7 @@ done
 
 
 #iptables -I INPUT -p tcp --match multiport --dport 30000:35000 -m state --state NEW -j ACCEPT
-function install() {
+function install_proxy() {
 	( # Install proxy server
 	  wget https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz &> /dev/null
 	  tar -xf 0.9.4.tar.gz
@@ -33,27 +33,28 @@ function start_proxy(){
 }
 
 function rotate(){
-	local main_ip6="$(ip -6 addr show dev eth0 scope global | grep -Po '(?<=inet6\s).+(?=/64\sscope\sglobal\snoprefixroute)')";
-	local main_ip4="$(ip -4 addr show dev eth0 scope global | grep -Po '(?<=inet\s).+(?=/24.+\sscope\sglobal\snoprefixroute)')";
+	interface_name="$(ip -br l | awk '$1 !~ "lo|vir|wl|@NONE" { print $1 }' | awk 'NR==1')";
+	main_ip6="$( ip -6 addr show dev $interface_name | grep -Po '(?<=inet6\s)(?!fe80).+(?=/64\sscope\s.+\snoprefixroute)' )";
+	main_ip4="$( ip -4 addr show dev $interface_name | grep -Po '(?<=inet\s).+(?=/\d+.+\sscope\s.+\snoprefixroute)' )";
 	#echo "$1";
 
-	readarray -t old_ips < <( ip -6 addr | grep -Po '(?<=inet6\s).+(?=/64\sscope\sglobal)');
+	readarray -t old_ips < <( ip -6 addr show dev $interface_name | grep -Po '(?<=inet6\s).+(?=/64\sscope\sglobal)' );
 	old_ips=("${old_ips[@]/$main_ip6}");
 	#printf '%s\n' "${old_ips[@]}";
 	#for ip in "${old_ips[@]}";do echo "$ip" ;done;
 	
-	prefix_ip="$(echo "$main_ip6" | cut -f1-4 -d':')";
+	prefix_ip="$( echo "$main_ip6" | cut -f1-4 -d':' )";
 	#echo "$prefix_ip";
 	
-	local new_ips=();
-	local new_ips_cfg=();
+	new_ips=();
+	new_ips_cfg=();
 	array=( 1 2 3 4 5 6 7 8 9 0 a b c d e f );
 	function rh () { echo "${array[$RANDOM%16]}"; }
 	for(( i=0; i<$1; i++ ));do
 		new_ip="$prefix_ip:$(rh)$(rh)$(rh)$(rh):$(rh)$(rh)$(rh)$(rh):$(rh)$(rh)$(rh)$(rh):$(rh)$(rh)$(rh)$(rh)";
 		new_ips+=($new_ip);
 		new_ips_cfg+=("proxy -6 -n -a -p$((30000+$i)) -i$main_ip4 -e$new_ip");
-		ip -6 addr add ${new_ip}/64 dev eth0;
+		ip -6 addr add $new_ip/64 dev $interface_name;
 	done;
 	
 	printf -v new_ips_cfg_string '%s\n' "${new_ips_cfg[@]}";
@@ -66,18 +67,25 @@ timeouts 1 5 30 60 180 1800 15 60
 auth none
 ${new_ips_cfg_string%,}">3proxy/cfg/3proxy.cfg;
 
-	for old_ip in "${old_ips[@]}";do ip -6 addr del $old_ip/64 dev eth0; done;
+	for old_ip in "${old_ips[@]}";do ip -6 addr del $old_ip/64 dev $interface_name; done;
 	
 	stop_proxy;
 	start_proxy;
 }
 
 if [ "$act" = "i" ]; then 
-	echo 'install_3proxy'; 
-	install_3proxy ; 
+	echo 'install_proxy'; 
+	install_proxy ; 
 elif [ "$act" = "r" ]; then 
 	echo 'rotate'; 
 	rotate $proxy_count;
 else 
 	echo 'do nothing'; 
 fi;
+
+#curl -sO https://raw.githubusercontent.com/vt89qn/lcproxy/main/lcproxy.sh && chmod +x lcproxy.sh 
+#bash lcproxy.sh -act i
+#bash lcproxy.sh -act r -count 1000
+
+#firewall-cmd --zone=public --add-port=30000-31000/tcp --permanent
+#firewall-cmd --reload
